4045b22047a213a9ffd80be85a7b4d4cb732c7e5
FarhanAmir306 <amirf3051@gmail.com>
2024-01-01 21:28:50 +0600

    comment

--- library_management_project/library_management_project/views.py
+++ library_management_project/library_management_project/views.py
@@ -16,3 +16,5 @@ def home(request, category_slug = None): # initially dhore nicchi je category_sl
         data = Post.objects.filter(category  = category) # post er category te category object bosiye filter korlam, ager data er sathe overright hoye jabe
     categories = CatagoryModel.objects.all() # sob category dekhabo
     return render(request, 'index.html', {'data' : data, 'category' : categories})
+
+#comment for git
